318554ee4e6f0681a515f6344fa6bb561d5d265f
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-09-08 02:36:51 +0530

    add style for color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import ColorBox from "./ColorBox";
-import './Palette.css'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
+import './Palette.css'
 
 class Palette extends Component {
   state = {
@@ -21,7 +21,9 @@ class Palette extends Component {
     ))
     return (
       <div className={"Palette"}>
-        <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
+        <div className="slider">
+          <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
+        </div>
         <div className="Palette-colors">
           {colorBoxes}
         </div>
