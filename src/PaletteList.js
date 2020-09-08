994990d403a197ef135e298d0981b9f8b4dbdb1c
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-09-08 09:01:36 +0530

    add basic styles to mini palettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
+import MiniPalette from "./MiniPalette";
 
 class PaletteList extends Component {
   render() {
     return (
       <div>
+        <MiniPalette/>
         {this.props.palettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette}/>
         ))}
       </div>
     );
